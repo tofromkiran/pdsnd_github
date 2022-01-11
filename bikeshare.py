0b3c3ce3f7c0b58edc8064b4af9a44f750dc17fc
tofromkiran <Y2GEATS@CMS.LOCAL>
2022-01-11 10:05:22 -0500

    refactor: Modified restart input value

--- bikeshare.py
+++ bikeshare.py
@@ -194,7 +194,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df,city)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart and verify more data with new filter values? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
